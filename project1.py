d9b12d0057ed9a24baecf3d06e77629966416f86
Soumitra Chowdhury <soumitrachowdhury003@gmail.com>
2024-10-10 22:15:22 +0600

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,5 @@
+# Band name generator
+print("Welcome to our band name generator")
+city=input("Where did you grow up?\n")
+pet=input("What is your pet name?\n")
+print("The band name could be - "+city+" "+pet)
